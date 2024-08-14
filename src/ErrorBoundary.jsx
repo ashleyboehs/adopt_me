5780827bd7055d7a0da2db8875f2b159b2b533af
Ashley Boehs <ashley@boehs.com>
2024-08-14 16:10:26 -0500

    Context

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 class ErrorBoundary extends Component {
   state = { hasError: false };
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError() {
     return { hasError: true };
   }
 
